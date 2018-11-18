a2611ce09323b690ffff5863eb73b0b0ecbe7596
paguz42 <pagottomanuel@gmail.com>
2018-11-18 01:45:07 +0100

    popolato database

--- blueh_db.sql
+++ blueh_db.sql
@@ -39,6 +39,7 @@ CREATE TABLE `rocket_travel` (
 
 LOCK TABLES `rocket_travel` WRITE;
 /*!40000 ALTER TABLE `rocket_travel` DISABLE KEYS */;
+INSERT INTO `rocket_travel` VALUES (1,1,'1975-01-21'),(2,2,'1977-08-16'),(3,3,'1978-08-09'),(4,1,'1983-06-07'),(5,2,'1984-04-27'),(6,3,'1990-03-18'),(7,1,'1994-04-14'),(8,2,'2004-01-27'),(9,3,'2004-12-31'),(10,1,'2008-12-05');
 /*!40000 ALTER TABLE `rocket_travel` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,7 +55,7 @@ CREATE TABLE `rockets` (
   `model` varchar(20) NOT NULL,
   `price` bigint(12) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,6 +64,7 @@ CREATE TABLE `rockets` (
 
 LOCK TABLES `rockets` WRITE;
 /*!40000 ALTER TABLE `rockets` DISABLE KEYS */;
+INSERT INTO `rockets` VALUES (1,' Space Shuttle',780009176),(2,' Falcon Heavy',696735153),(3,'Falcon 9',410476496);
 /*!40000 ALTER TABLE `rockets` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -79,7 +81,7 @@ CREATE TABLE `travels` (
   `arrival` varchar(30) NOT NULL,
   `date` date DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,6 +90,7 @@ CREATE TABLE `travels` (
 
 LOCK TABLES `travels` WRITE;
 /*!40000 ALTER TABLE `travels` DISABLE KEYS */;
+INSERT INTO `travels` VALUES (1,' Pluto',' Mars','1983-06-07'),(2,'Cape Canaveral',' Moon','2004-12-31'),(3,'Cape Canaveral',' Pluto','1994-04-14'),(4,' Mars',' Mars','2004-01-27'),(5,'Cape Canaveral',' Pluto','1975-01-21'),(6,' Pluto',' Mars','1978-08-09'),(7,' Moon',' Moon','1977-08-16'),(8,'Cape Canaveral',' Mars','1990-03-18'),(9,' Moon',' Mars','1984-04-27'),(10,'Cape Canaveral',' Pluto','2008-12-05');
 /*!40000 ALTER TABLE `travels` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -106,7 +109,7 @@ CREATE TABLE `users` (
   `email` varchar(35) NOT NULL,
   `password` varchar(8) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -115,6 +118,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Zelma','Treutel','N.D.','conrad92@example.org','3e44fe44'),(2,'Jodie','Gleichner','M','amira12@example.com','c611cc0c'),(3,'Margarette','Quigley','F','dking@example.org','6450f4ed'),(4,'Twila','Hodkiewicz','N.D.','ekeeling@example.org','dd54d377'),(5,'Sonny','Gutkowski','N.D.','dicki.natasha@example.org','9a072b5d'),(6,'Dennis','Casper','F','anabelle.reichert@example.net','04b67e1b'),(7,'Guadalupe','Botsford','N.D.','adele.smitham@example.org','5b446224'),(8,'Eduardo','Altenwerth','M','freddie34@example.com','3254bd29'),(9,'Ralph','Mueller','N.D.','taryn87@example.net','20d05139'),(10,'Idella','Orn','F','ldare@example.org','11f42e4b');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -127,4 +131,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-17 20:02:47
+-- Dump completed on 2018-11-18  1:44:39
