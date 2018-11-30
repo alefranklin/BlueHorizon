43febfc746b9df38c1a144c97fcf36c1053c4e2e
Matteo Pagotto <matteopagotto2@gmail.com>
2018-11-30 01:07:27 +0100

    close #6

--- utils/connessione_db.php
+++ utils/connessione_db.php
@@ -1,13 +1,78 @@
 <?php
-    //connessione al database
+    /***************************************** connessione al database ****************************************************/
     include("config.php"); 
 
-    $db= new mysqli($host,$db_user,$db_psw, $db_name);  // ip locale, login, password e nome database
-    if(!$db){
-        $msg='Non riesco a connettermi: errore '.mysql_error(); // questo apparirà solo se ci sarà un errore
-        die ($msg);
-        error_log("'$msg'\n", 3, $log_file);
+    // ip locale, login, password e nome database
+    $db = new mysqli($host,$db_user,$db_psw, $db_name) or die ('Non riesco a connettermi: errore '.mysql_error());
+    
+    // rendo globale db per poterlo usare nelle funzioni
+    $GLOBALS['db'] = $db;
+
+
+    /***************************************** funzioni utili *************************************************************/
+
+    function check_username($username) {
+        
+        $db = $GLOBALS['db'];
+        
+        $ris = $db->query("SELECT * FROM users WHERE username = '$username'") or die (mysql_error());  
+        if($ris->fetch_assoc() == NULL) return 0;
+        else return 1;
     }
 
-    error_log("database connesso con successo\n", 3, $log_file);
+    function check_email($email) {
+        
+        $db = $GLOBALS['db'];
+        
+        $ris = $db->query("SELECT * FROM users WHERE email = '$email'") or die (mysql_error());  
+        if($ris->fetch_assoc() == NULL) return 0;
+        else return 1;
+    }
+
+    function exist($email, $passwd) {
+        
+        $db = $GLOBALS['db'];
+        
+        // sha256 della password in questo modo corrisponde con quella del database
+        $hash_passwd = myhash($passwd);
+        
+        $query = "SELECT * FROM users WHERE email = '$email' AND password = '$hash_passwd' ";
+        $ris = $db->query($query) or die (mysql_error());
+        $data=$ris->fetch_assoc();  
+        
+        if($data == NULL) return 0;
+        else return $data;
+    }
+
+    function get_user($email, $passwd) {
+        
+        $db = $GLOBALS['db'];
+        
+        if($user = exist($email, $passwd)) {
+            
+            // salvo i dati dello user in session
+            $_SESSION['user'] = $user;
+
+            // registro l'autorizzazione dell'utente ad accedere alle aree riservate
+            $_SESSION["autorizzato"] = 1;
+            
+            // controllo se l'utente è admin
+            if($user['id'] == 1 and $user["name"] == "admin" ) $_SESSION['admin'] = 1;
+            else $_SESSION['admin'] = 0;
+            
+            return 1;
+
+        } else {
+            
+            // nessun accesso al sito
+            $_SESSION["autorizzato"] = 0;
+            $_SESSION['admin'] = 0;
+            return 0;
+        }
+        
+    }
+    
+    function myhash($str) {
+        return hash("sha256", $str."salt");
+    }
 ?>
