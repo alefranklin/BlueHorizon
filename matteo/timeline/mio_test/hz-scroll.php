6c350087a1cac4aab479027d4586efb70f256b25
Matteo Pagotto <matteopagotto2@gmail.com>
2018-12-01 00:51:11 +0100

    puttanate aggiunte

---
+++ matteo/timeline/mio_test/hz-scroll.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="timeline.css">
+</head>
+<body>
+    <div class="timeline">
+        <?php include("../solo_css/hz-scroll.php"); ?>
+    </div>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="jquery.jInvertScroll.js"></script>
+</body>
+</html>
+
