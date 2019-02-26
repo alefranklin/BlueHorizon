73b9fe6807867c332e8a033b0ad2728ed2c3fe50
Stefano Baggio <baggiostefano27@gmail.com>
2019-02-26 16:40:52 +0100

    slideshow: first sketchy attempt

--- index.php
+++ index.php
@@ -21,11 +21,11 @@
 <div id="body-page" class="" role="main">
     <div id="presentation">
         <h1 class="space-font">Welcome space wanderer!</h1>
-        <p class="space-font" class="mySlides"><img src="./img/Slideshow_1.jpg" alt="Sshow1">Join our space travel community!</p>
-        <p class="space-font" class="mySlides"><img src="./img/Slideshow_2.jpg" alt="Sshow2">See our space-ships!</p>
-        <p class="space-font" class="mySlides"><img src="./img/Slideshow_3.jpg" alt="Sshow3">Learn more about us!</p>
-        <p class="space-font" class="mySlides"><img src="./img/Slideshow_4.jpg" alt="Sshow4">Book the flight of your dreams among our plentiful travel options</p>
-        <p class="space-font" class="mySlides"><img src="./img/Slideshow_5.jpg" alt="Sshow5">Take a look at some neat facts about the planets you're going to orbit around!</p>
+        <div class="mySlides"><img src="./img/Slideshow_1.jpg" alt="Sshow1">Join our space travel community!</div>
+        <div class="mySlides"><img src="./img/Slideshow_2.jpg" alt="Sshow2">See our space-ships!</div>
+        <div class="mySlides"><img src="./img/Slideshow_3.jpg" alt="Sshow3">Learn more about us!</div>
+        <div class="mySlides"><img src="./img/Slideshow_4.jpg" alt="Sshow4">Book the flight of your dreams among our plentiful travel options</div>
+        <div class="mySlides"><img src="./img/Slideshow_5.jpg" alt="Sshow5">Take a look at some neat facts about the planets you're going to orbit around!</div>
     </div>
 </div>
 
@@ -42,7 +42,7 @@ function carousel() {
   myIndex++;
   if (myIndex > x.length) {myIndex = 1}    
   x[myIndex-1].style.display = "block";  
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  setTimeout(carousel, 4000); // Change image every 2 seconds
 }
 </script>
 
