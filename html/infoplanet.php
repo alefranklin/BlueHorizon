b6aec7a6dae626e1659039a969b1cb63e742cad5
Stefano Baggio <baggiostefano27@gmail.com>
2019-03-12 16:31:29 +0100

    Missing travel panels added

--- html/infoplanet.php
+++ html/infoplanet.php
@@ -22,52 +22,65 @@
     <div id="info-content">
         <h1>Choose a planet!</h1>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/mercurio-info.png" alt="mercurio" /></a>
+            <a href="#info-text"><img src="../img/mercurio-info.png" alt="mercurio" />
             <h1>Mercury</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/venere-info.png" alt="venere" /></a>
+            <a href="#info-text"><img src="../img/venere-info.png" alt="venere" />
             <h1>Venus</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/terra-info.png" alt="terra" /></a>
+            <a href="#info-text"><img src="../img/terra-info.png" alt="terra" />
             <h1>Earth</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/luna-info.png" alt="luna" /></a>
+            <a href="#info-text"><img src="../img/luna-info.png" alt="luna" />
             <h1>Moon</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/marte-info.png" alt="marte" /></a>
+            <a href="#info-text"><img src="../img/marte-info.png" alt="marte" />
             <h1>Mars</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/giove-info.png" alt="giove" /></a>
+            <a href="#info-text"><img src="../img/giove-info.png" alt="giove" />
             <h1>Jupiter</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/io-info.png" alt="io" /></a>
+            <a href="#info-text"><img src="../img/io-info.png" alt="io" />
             <h1>Io</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/europa-info.png" alt="europa" /></a>
+            <a href="#info-text"><img src="../img/europa-info.png" alt="europa" />
             <h1>Europa</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/saturno-info.png" alt="saturno" /></a>
+            <a href="#info-text"><img src="../img/saturno-info.png" alt="saturno" />
             <h1>Saturn</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/nettuno-info.png" alt="nettuno" /></a>
+            <a href="#info-text"><img src="../img/nettuno-info.png" alt="nettuno" />
             <h1>Neptune</h1>
+            </a>
         </div>
         <div class="info-tile">
-            <a href="#info-text"><img src="../img/plutone-info.png" alt="plutone" /></a>
+            <a href="#info-text"><img src="../img/plutone-info.png" alt="plutone" />
             <h1>Pluto</h1>
+            </a>
         </div>
 
         <div id="info-text">
-            <p>prova</p>
+            <h1>Planet Name</h1>
+            <a href="infoplanet.php">X</a>
+            <p>texttexttexttexttexttexttexttexttexttext</p>
         </div>
 
     </div>
--- html/travels.php
+++ html/travels.php
@@ -23,23 +23,80 @@
     <h1 id="travels-title" class="space-font">Our Travels</h1>
 
     <div id="travel-list">
-      <a href="planets.php?planet=Marte" title="See more infos about our Mars trip">
-        <div class="travel-panel" id="mars-banner">
-            <h1 class="space-font"> MARTE </h1>
+
+      <a href="planets.php?planet=Mercurio" title="See more infos about our Mercury trip">
+        <div class="travel-panel" id="mercury-banner">
+            <h1 class="space-font"> MERCURY </h1>
             <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
         </div>
       </a>
-        <br>
-      <a href="planets.php?planet=Plutone" title="See more infos about our Pluton trip">
-        <div class="travel-panel" id="pluto-banner">
-            <h1 class="space-font"> PLUTONE </h1>
+      <br>
+      <a href="planets.php?planet=Venere" title="See more infos about our Venus trip">
+        <div class="travel-panel" id="venus-banner">
+            <h1 class="space-font"> VENUS </h1>
             <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
         </div>
       </a>
-        <br>
+      <br>
       <a href="planets.php?planet=Terra" title="See more infos about our Earth trip">
         <div class="travel-panel" id="earth-banner">
-            <h1 class="space-font"> TERRA </h1>
+            <h1 class="space-font"> EARTH </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </a>
+      <br>
+      <a href="planets.php?planet=Luna" title="See more infos about our Moon trip">
+        <div class="travel-panel" id="moon-banner">
+            <h1 class="space-font"> MOON </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </a>
+      <br>
+      <a href="planets.php?planet=Marte" title="See more infos about our Mars trip">
+        <div class="travel-panel" id="mars-banner">
+            <h1 class="space-font"> MARS </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </a>
+      <br>
+      <a href="planets.php?planet=Giove" title="See more infos about our Jupiter trip">
+        <div class="travel-panel" id="jupiter-banner">
+            <h1 class="space-font"> JUPITER </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </a>
+      <br>
+      <a href="planets.php?planet=Io" title="See more infos about our Io trip">
+        <div class="travel-panel" id="io-banner">
+            <h1 class="space-font"> IO </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </a>
+      <br>
+      <a href="planets.php?planet=Europa" title="See more infos about our Europa trip">
+        <div class="travel-panel" id="europa-banner">
+            <h1 class="space-font"> EUROPA </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </a>
+      <br>
+      <a href="planets.php?planet=Saturno" title="See more infos about our Saturn trip">
+        <div class="travel-panel" id="saturn-banner">
+            <h1 class="space-font"> SATURN </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </a>
+      <br>
+      <a href="planets.php?planet=Nettuno" title="See more infos about our Neptune trip">
+        <div class="travel-panel" id="neptune-banner">
+            <h1 class="space-font"> NEPTUNE </h1>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
+        </div>
+      </a>
+      <br>
+      <a href="planets.php?planet=Plutone" title="See more infos about our Pluton trip">
+        <div class="travel-panel" id="pluto-banner">
+            <h1 class="space-font"> PLUTON </h1>
             <p> Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. </p>
         </div>
       </a>
