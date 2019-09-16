a99aba639b127df4b596587712abc15287cc5b1f
Stefano Baggio <baggiostefano27@gmail.com>
2019-09-16 16:30:13 +0200

    daghe gas un colpo

--- html/rockets.php
+++ html/rockets.php
@@ -20,34 +20,57 @@
 </div>
 
 <div id="body-page" class="">
+    <div id="rockets">
     <h1><?php tr("Our spacecrafts") ?></h1>
+    <a href="ship.php?ship=Millennium-Balcon" title="See more infos about Millennium-Balcon">
+        <div class="rocket-panel" id="millennium-banner">
+            <h2 class="space-font"> <?php tr("MILLENNIUM BALCON") ?> </h1>
+        </div>
+      </a>
+      <br>
+      <a href="ship.php?ship=StarBooster" title="See more infos about StarBooster">
+        <div class="rocket-panel" id="starbooster-banner">
+            <h2 class="space-font"> <?php tr("STARBOOSTER") ?> </h1>
+            </div>
+      </a>
+      <br>
+      <a href="ship.php?planet=Terra" title="See more infos about Arcasia">
+        <div class="rocket-panel" id="arcasia-banner">
+            <h2 class="space-font"> <?php tr("ARCASIA") ?> </h1>
+            </div>
+      </a>
+      <br>
+    </div>  
+</div>
+
+<!--
     <div id="rockets">
 
         <dl>
-            <dt><?php tr("Name 1") ?></dt>
+            <dt><//?php tr("Name 1") ?></dt>
             <dd>
                 <img class="space-ship" src="../img/hyperion-shuttle.jpg" alt=<?php tr("BluHorizon's spacecrafts-1") ?>/>
-                <p> <?php tr("qui ci va la descrizione del razzo") ?>
+                <p> <//?php tr("qui ci va la descrizione del razzo") ?>
                 </p>
             </dd>
 
-            <dt><?php tr("Name 2") ?></dt>
+            <dt><//?php tr("Name 2") ?></dt>
             <dd>
                 <img class="space-ship" src="../img/ship2.jpg" alt=<?php "BluHorizon's spacecrafts-2" ?>/>
-                <p><?php tr("qui ci va la descrizione del razzo") ?>
+                <p><//?php tr("qui ci va la descrizione del razzo") ?>
                 </p>
             </dd>
 
-            <dt><?php tr("Name 3") ?></dt>
+            <dt><//?php tr("Name 3") ?></dt>
             <dd>
                 <img class="space-ship" src="../img/ship3.jpeg" alt=<?php "BluHorizon's spacecrafts-3" ?>/>
-                <p><?php tr("qui ci va la descrizione del razzo") ?>
+                <p><//?php tr("qui ci va la descrizione del razzo") ?>
                 </p>
             </dd>
         </dl>
     </div>
 
-</div>
+ -->
 
 <!-- footer -->
 <?php include($local_path."html/footer.php"); ?>
